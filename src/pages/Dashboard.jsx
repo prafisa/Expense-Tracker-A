7b44c215b6b68f9b32c2ecbc3934acf122adc2f7
Swastika Dangol <dangolswastika2018@gmail.com>
2026-04-20 12:55:42 +0545

    remove leftmargin in dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ import RecentTransactions from "../components/Dashboard/RecentTransactions";
 export default function Dashboard() {
     console.log("categoryData:", data.categoryData)
   return (
-    <div className="mx-5 bg-gray-50 p-6" style={{marginLeft:250}}>
+    <div className="mx-5 bg-gray-50 p-6">
 
       {/* Header */}
       <div className="flex justify-between items-center mb-6">
