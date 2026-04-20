8c2b0a2aa4c1579dadf80d7f840b449f41ae3ed7
shiwaniii <shiwani.adhikari@gmail.com>
2026-04-20 11:52:18 +0545

    transaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-function App() {
+import { useState } from "react";
+import TransactionForm from "./components/TransactionForm";
+import TransactionList from "./components/TransactionList";
+
+export default function App() {
+  const [transactions, setTransactions] = useState([]);
+
+  const addTransaction = (tx) => {
+    setTransactions([...transactions, { ...tx, id: Date.now() }]);
+  };
+
   return (
-    <>
-      <p className='text-red-500'>Expense tracker</p>
-    </>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-5">
+      <div className="bg-white shadow-xl rounded-2xl w-full max-w-md p-6">
+        <h1 className="text-2xl font-bold text-center mb-4">
+           Transaction Tracker
+        </h1>
+
+        <TransactionForm addTransaction={addTransaction} />
+        <TransactionList transactions={transactions} />
+      </div>
+    </div>
   );
 }
-
-export default App;
